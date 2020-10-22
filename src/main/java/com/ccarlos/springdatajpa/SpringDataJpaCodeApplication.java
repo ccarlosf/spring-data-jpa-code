f83ac59f3242af74c994a41fce45533cb6accb5d
ccarlosf <2238513100@qq.com>
2020-10-22 09:50:10 +0800

    Initial commit

---
+++ src/main/java/com/ccarlos/springdatajpa/SpringDataJpaCodeApplication.java
@@ -0,0 +1,13 @@
+package com.ccarlos.springdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ccarlos/springdatajpa/SpringDataJpaCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccarlos.springdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
